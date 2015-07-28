19a19d0a3bef31b40ed15f9a57b6f83ea285dd2a
Alex Lyadinskii <alyadins@gmail.com>
2015-07-29 00:39:58 +0300

    search activity view

---
+++ app/src/main/java/com/example/tetest/SearchActivity.java
@@ -0,0 +1,23 @@
+package com.example.tetest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+    }
+}
